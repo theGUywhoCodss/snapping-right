8bcdf706f04e335d813808a7d669d852bdf3c6b9
theGUywhocodss <cooool1324@gmail.com>
2024-01-28 12:04:50 -0600

    updated header

--- source/headers/node.h
+++ source/headers/node.h
@@ -3,4 +3,7 @@
 void generateNodes(Vector2 scrDimensions);
 void drawNodes();
 void runNodes();
+void nodeSetterXY(int node,bool mode);
+int getNodeXY(int x,int y);
+void unaliveNode(int node);
 #endif
